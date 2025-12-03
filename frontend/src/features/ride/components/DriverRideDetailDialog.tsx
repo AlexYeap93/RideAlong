ceb9608682c888c376c01ad4cc8036ec53940562
MD Shaherier Khan <mdshaherier.khan@ucalgary.ca>
2025-12-02 22:58:12 -0700

    Move available time slots to const.ts and import in DriverRideDetailDialog

--- frontend/src/features/ride/components/DriverRideDetailDialog.tsx
+++ frontend/src/features/ride/components/DriverRideDetailDialog.tsx
@@ -12,6 +12,7 @@ import { toast } from "sonner";
 import { bookingsAPI, usersAPI } from "../../../services/api";
 import { useAuth } from "../../../contexts/AuthContext";
 import { DriverRideDetailDialogProps } from "../../../types";
+import { timeSlots } from "../../../types/const";
 
 
 export function DriverRideDetailDialog({ 
@@ -62,19 +63,7 @@ export function DriverRideDetailDialog({
     setPassengerPickupTimes(initialTimes);
   }, [passengers, departureTime]);
 
-  // Available time slots for the ride
-  const timeSlots = [
-    "8:00 AM", "8:15 AM", "8:30 AM", "8:45 AM", "9:00 AM",
-    "9:15 AM", "9:30 AM", "9:45 AM", "10:00 AM", "10:15 AM",
-    "10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM",
-    "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM",
-    "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM", "2:00 PM",
-    "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "3:15 PM",
-    "3:30 PM", "3:45 PM", "4:00 PM", "4:15 PM", "4:30 PM",
-    "4:45 PM", "5:00 PM", "5:15 PM", "5:30 PM", "5:45 PM",
-    "6:00 PM", "6:15 PM", "6:30 PM", "6:45 PM", "7:00 PM",
-    "7:15 PM", "7:30 PM", "7:45 PM", "8:00 PM"
-  ];
+  
 
   const handlePickupTimeChange = (passengerId: number | string, newTime: string) => {
     setPassengerPickupTimes(prev => ({
